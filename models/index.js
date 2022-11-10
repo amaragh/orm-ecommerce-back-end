0c17d353d7121bf60aa3550c127c1cd40b06cfce
amaragh <mercer.alecia@gmail.com>
2022-11-09 19:38:23 -0600

    add product routes

--- models/index.js
+++ models/index.js
@@ -18,14 +18,14 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tagged_products',
+  as: 'tags',
   foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'tagged_products',
+  as: 'tags',
   foreignKey: 'tag_id'
 });
 
